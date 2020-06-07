63a2286a2e817492bd8785bc705087419be84d19
rohit10000 <rohitsingh18997@gmail.com>
2020-06-08 03:54:06 +0530

    my first commit for springBoot

---
+++ src/main/java/dev/rohit/springproject/SpringprojectApplication.java
@@ -0,0 +1,14 @@
+package dev.rohit.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringprojectApplication.class, args);
+        System.out.println("Hi Rohit!");
+    }
+
+}
